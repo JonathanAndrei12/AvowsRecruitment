8b22d3224fd08ab001f92d2e3b32f766b9ef43cc
JonathanAndrei12 <jonathanandreiwibawa@gmail.com>
2022-09-12 18:00:00 +0700

    Initial Commit

---
+++ Avows Recruitment/Avows_RecruitmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avows_RecruitmentApp.swift
+//  Avows Recruitment
+//
+//  Created by Jonathan Andrei on 12/09/22.
+//
+
+import SwiftUI
+
+@main
+struct Avows_RecruitmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
